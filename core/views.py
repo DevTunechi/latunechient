4ee04715c82d4e0c151c4cae5a7d9a8032c7f50a
Olatunji Oluwadare <tunjidare2@yahoo.com>
2025-04-21 19:48:19 +0100

    All up to date

--- core/views.py
+++ core/views.py
@@ -6,5 +6,14 @@ def home(request):
 def about(request):
     return render(request, 'core/about.html')
 
+def gallery(request):
+    return render(request, 'core/gallery.html')
+
 def contact(request):
     return render(request, 'core/contact.html')
+
+def projects(request):
+    return render(request, 'core/projects.html')
+
+def contact_form(request):
+    return render(request, 'core/contact_form.html')
