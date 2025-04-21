91cfed20d42c55a7b051309dd28196a763c275cd
Olatunji Oluwadare <tunjidare2@yahoo.com>
2025-04-21 19:12:06 +0100

    untracked files

--- latunechient/urls.py
+++ latunechient/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
     path('', include('core.urls')),
 ]
