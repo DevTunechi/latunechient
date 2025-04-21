8cb74efadf1a3c126a2230ce4cd91e578b1dc73e
Olatunji Oluwadare <tunjidare2@yahoo.com>
2025-04-21 14:28:47 +0100

    lost track but adjusted basic html files and restarted server

--- latunechient/settings.py
+++ latunechient/settings.py
@@ -121,6 +121,11 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / 'core/static']
 MEDIA_ROOT = BASE_DIR / 'media'
 
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
